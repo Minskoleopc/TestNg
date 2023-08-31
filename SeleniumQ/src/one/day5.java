f2b0a0c1b68f56986f670e44df3b28dc1e800e7b
Minskoleopc <minskoleopc@gmail.com>
2023-08-31 08:07:15 +0530

    added

--- SeleniumQ/src/one/day5.java
+++ SeleniumQ/src/one/day5.java
@@ -4,7 +4,19 @@ public class day5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// driver.close()
+		// driver.quit()
+		// driver.getTitle()
+		// driver.getCurrentUrl()
+		// driver.navigate()
+		// driver.getPageSorce()
+		// driver.findElement()
+		// driver.findElements()
+		
+		
+		
+		
 	}
 
 }
