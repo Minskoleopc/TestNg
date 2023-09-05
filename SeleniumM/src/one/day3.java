79c6f6fafcd3d7b0e218f96911c364bb73f90cc0
Minskoleopc <minskoleopc@gmail.com>
2023-09-05 18:05:34 +0530

    added

---
+++ SeleniumM/src/one/day3.java
@@ -0,0 +1,78 @@
+package one;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+		WebElement contactE =	driver.findElement(By.cssSelector("#contact_me > div > div:nth-child(1) > div > h2"));
+		// isDisplayed()
+//		boolean a = contactE.isDisplayed();
+//		if(a) {
+//			System.out.println("test case pass");
+//		}
+//		else {
+//			System.out.println("test case fail");
+//		}
+		
+		// getText()
+//		if(contactE.getText().equals("CONTACT US")) {
+//			System.out.println("Testcase pass 2");
+//		}
+//		else {
+//			System.out.println("Testcase Fail 2");
+//		}
+		
+		//sendKeys();
+		WebElement firstName = driver.findElement(By.cssSelector("#contact_form > input:nth-child(1)"));
+		WebElement lastName = driver.findElement(By.cssSelector("#contact_form > input:nth-child(2)"));
+		WebElement email = driver.findElement(By.cssSelector("#contact_form > input:nth-child(3)"));
+		WebElement message = driver.findElement(By.cssSelector("#contact_form > textarea"));
+		WebElement submitButton = driver.findElement(By.cssSelector("#form_buttons > input:nth-child(2)"));
+		// clear
+		//firstName.clear();
+		//click()
+//		firstName.sendKeys("mayuri");
+//		lastName.sendKeys("rao");
+//		email.sendKeys("mayurirao03@mail.com");
+//		message.sendKeys("learning new thinsg");
+//		submitButton.click();
+//	
+		
+		// submit()
+		firstName.sendKeys("mayuri");
+		lastName.sendKeys("rao");
+		email.sendKeys("mayurirao03@mail.com");
+		message.sendKeys("learning new thinsg");
+		submitButton.submit();
+		
+		// click() , sendKeys() , clear() , submit() , isDisplayed() , getText();
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+		
+		
+
+	}
+
+}
