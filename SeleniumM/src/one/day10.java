6311759faf82b4ce75a10b2d480a58ff9e3fc079
Minskoleopc <minskoleopc@gmail.com>
2023-09-22 09:42:43 +0530

    added

---
+++ SeleniumM/src/one/day10.java
@@ -0,0 +1,47 @@
+package one;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ankurjain\\Desktop\\chromedriver\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.webdriveruniversity.com");
+		driver.findElement(By.cssSelector("#contact-us")).click();
+		
+		String parentWindow = driver.getWindowHandle();
+		System.out.println(parentWindow);
+		
+		Set <String>windowIds = driver.getWindowHandles();
+		System.out.println(windowIds);
+		
+		
+		for(String id:windowIds) {
+			System.out.println(id);
+			if(id != parentWindow) {
+				driver.switchTo().window(id);
+				System.out.println(driver.getCurrentUrl());
+			}
+		}
+		
+		
+		boolean ele = driver.findElement(By.cssSelector("#contact_me > div > div:nth-child(1) > div > h2")).isDisplayed();
+		System.out.println(ele);
+	
+		driver.switchTo().window(parentWindow);
+		System.out.println(driver.getCurrentUrl());
+		
+		
+		
+		
+	}	
+	
+
+}
